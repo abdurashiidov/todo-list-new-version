e125ba61fdaac281acfe6da27b040a5733f0fc96
abdurashiidov <abdurashiidov18@gmail.com>
2022-11-17 21:41:07 +0200

    commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const input = document.querySelector(".input")
+const input2 = document.querySelector(".input2")
+const btn = document.querySelector(".btn")
+const list = document.querySelector(".list")
+
+let newArr = [];
+
+btn.addEventListener("click", ()=>{
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+
+    list.appendChild(span)
+    list.appendChild(li)
+    
+    li.textContent = input.value
+    li.classList = "lii"
+
+    let neww = Date.now()
+    span.textContent = neww
+
+    newArr.push(input.value)
+
+    // console.log(li.length);
+
+    // for (let i = 0; i < li.length; i++) {
+    //     const element = array[i];
+    //     console.log(element.length);
+        
+    // }
+
+    input.value = ""
+})
+
+
+// input2.addEventListener("change", (e)=>{
+//     console.log("ok");
+    
+
+//     if (input2.value.length == 0) {
+//         btn.disabled = true;
+//     } else {
+//         console.log("ok");
+//     }
+// })
